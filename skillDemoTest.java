eaec2d784ce0968974cbe84adddde0404c62e3c9
sergioferniza <91352161+sergioferniza@users.noreply.github.com>
2022-05-04 20:57:32 -0700

    initial changes
    these should not work correctly.

---
+++ skillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTest {  
+    @Test
+    public void division(){
+        assertEquals(0, skillDemo.division(1, 0));
+    }
+}
