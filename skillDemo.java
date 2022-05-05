7523fa411706b097dbb9ade5a3979eb9c9b8fb6c
sergioferniza <91352161+sergioferniza@users.noreply.github.com>
2022-05-04 20:53:37 -0700

    Class created.
    i've created my main java class.

---
+++ skillDemo.java
@@ -0,0 +1,9 @@
+public class skillDemo{
+
+  /// this method will calculate a division with two int imputs.
+  /// if the denominator is equal to 0 it will return 0.
+
+  public static int division(int x, int y){
+    return x/y;
+  }
+}
